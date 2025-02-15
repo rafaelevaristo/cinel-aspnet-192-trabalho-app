3e592e0a37feaaffe33ffa63d5afd8898535844b
rafael.evaristo <rafael.evaristo@gmail.com>
2025-02-15 13:16:32 +0000

    Added view and model for consolidated values.

---
+++ waap/waap/ViewModels/Reporting/Saldo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace waap.ViewModels.Reporting
+{
+    public class SaldoViewModel
+    {
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Final value must be greater than 0.")]
+        public decimal TotalPaidValue { get; set; }
+        public decimal TotalSalesValue { get; set; }
+        public decimal TotalNonPaidValue { get; set; }
+
+    }
+}
